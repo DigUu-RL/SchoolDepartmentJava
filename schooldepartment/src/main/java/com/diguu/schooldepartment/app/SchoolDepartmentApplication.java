2001a8c148e21a3901d61283aa09aef1d1cdeb29
Rodrigo Geribola <rodrigogeribola@hotmail.com>
2023-02-24 08:46:11 -0300

    resolving dependency injection

--- schooldepartment/src/main/java/com/diguu/schooldepartment/app/SchoolDepartmentApplication.java
+++ schooldepartment/src/main/java/com/diguu/schooldepartment/app/SchoolDepartmentApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
-@EnableJpaRepositories("com.diguu.schooldepartment.app.infra.*")
 public class SchoolDepartmentApplication {
 
 	public static void main(String[] args) {
